ba64a88b0890ae7e30899c6f36475502f871e204
Adi957 <adityaraj05494054@gmail.com>
2024-08-01 22:14:08 +0530

    Initial commit with Flutter project

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home:
-          SplashScreenHandler(), // Use SplashScreenHandler to manage splash and navigation
+          SplashScreenHandler(), // Using SplashScreenHandler to manage splash and navigation
     );
   }
 }
